b61fffa7121afb480b592c497647931b09e686dd
tomascastillo <castillotomas96@gmail.com>
2020-06-07 18:39:00 -0300

    Initial commit

---
+++ src/ecnryptation/Encryptation.java
@@ -0,0 +1,11 @@
+package ecnryptation;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Encryptation {
+
+	public static void main(String[] args) {
+	System.out.println(new BCryptPasswordEncoder().encode(("1234")));
+	}
+
+}
